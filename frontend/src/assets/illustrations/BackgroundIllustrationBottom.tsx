0f6e867a29ce20347d9395d5cd716d8330b9eb0a
aybeedee <abdullahumer575@gmail.com>
2024-05-09 22:05:12 +0500

    feat: login page functional ui

--- frontend/src/assets/illustrations/BackgroundIllustrationBottom.tsx
+++ frontend/src/assets/illustrations/BackgroundIllustrationBottom.tsx
@@ -1,11 +1,11 @@
 export default function BackgroundIllustrationBottom() {
   return (
     <svg width="800" height="264" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="400" cy="400" r="400" fill="url(#footerglow_paint0_radial)" fill-opacity=".4"></circle>
+      <circle cx="400" cy="400" r="400" fill="url(#footerglow_paint0_radial)" fillOpacity=".4"></circle>
       <defs>
         <radialGradient id="footerglow_paint0_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="rotate(90 0 400) scale(315.089)">
-          <stop stop-color="#3ABAB4"></stop>
-          <stop offset="1" stop-color="#3ABAB4" stop-opacity=".01"></stop>
+          <stop stopColor="#3ABAB4"></stop>
+          <stop offset="1" stopColor="#3ABAB4" stopOpacity=".01"></stop>
         </radialGradient>
       </defs>
     </svg>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,17 +3,52 @@ import { BackgroundIllustrationBottom, BackgroundIllustrationTop } from "@/asset
 export default function Login() {
   return (
     <div className="w-screen h-screen flex flex-row bg-[#18181a]">
-      <div className="w-1/2 relative overflow-hidden border border-red-500">
+      <div className="w-1/2 relative overflow-hidden">
         <div className="absolute top-0 right-0 opacity-60 translate-x-1/2">
           <BackgroundIllustrationTop />
         </div>
         <div className="absolute bottom-0 left-0 opacity-60 -translate-x-1/2">
           <BackgroundIllustrationBottom />
         </div>
-        <h1 className="text-white text-4xl">Left</h1>
+        <div className="flex flex-col gap-4 border border-red-500 h-full justify-center items-center">
+          <h1 className="text-white font-semibold text-4xl text-center">
+            Login to Your Account
+          </h1>
+          <p className="text-white text-lg text-center font-light">
+            Welcome Back! Login wtih your credentials to access all features of CMDb.
+          </p>
+          <div className="flex justify-center items-center gap-4">
+            <p className="text-white text-lg font-light">New Here?</p>
+            <button className="bg-white w-fit px-4 py-1 rounded-sm">Sign Up</button>
+          </div>
+        </div>
       </div>
-      <div className="w-1/2 text-white text-6xl font-bold">
-        Your website, reimagined
+      <div className="w-1/2 flex flex-col items-center border border-red-500">
+        <h1 className="text-white text-3xl font-semibold">Sign In</h1>
+        <form className="flex flex-col">
+          <div className="flex flex-col">
+            <label className="text-white font-light">Email</label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              required={true}
+              className="px-2 py-1"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white font-light">Password</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              required={true}
+              className="px-2 py-1"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-white w-fit px-4 py-1 rounded-sm"
+          >Login</button>
+        </form>
       </div>
     </div>
   );
