5a8bb6ac02676ee774bbed5aaf95fd20d039695d
aybeedee <abdullahumer575@gmail.com>
2024-05-14 15:22:14 +0500

    feat: complete seed schema

--- backend/src/config/seed.sql
+++ backend/src/config/seed.sql
@@ -10,10 +10,37 @@ CREATE TABLE users(
 
 CREATE TABLE movies(
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  title VARCHAR(255) NOT NULL,
+  title VARCHAR(255) UNIQUE NOT NULL,
+  description TEXT NOT NULL,
+  duration_hours INTEGER NOT NULL,
+  duration_minutes INTEGER NOT NULL,
+  review_count INTEGER NOT NULL DEFAULT 0,
+  poster_url TEXT NOT NULL,
+  trailer_url TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
   user_id UUID NOT NULL,
-  CONSTRAINT fk_users
+  CONSTRAINT fk_movies_users
     FOREIGN KEY(user_id)
       REFERENCES users(id)
       ON DELETE CASCADE
+);
+
+CREATE TABLE reviews (
+  user_id UUID NOT NULL,
+  movie_id UUID NOT NULL,
+  comment TEXT NOT NULL,
+  rating INTEGER NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  edited BOOLEAN NOT NULL DEFAULT false,
+  PRIMARY KEY (user_id, movie_id)
+  CONSTRAINT fk_reviews_users
+    FOREIGN KEY(user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE
+  CONSTRAINT fk_reviews_movies
+    FOREIGN KEY(movie_id)
+      REFERENCES movies(id)
+      ON DELETE CASCADE
+  CONSTRAINT check_rating_range
+    CHECK (rating BETWEEN 0 AND 5)
 );
