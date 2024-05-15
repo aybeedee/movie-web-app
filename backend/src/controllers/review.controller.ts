53518400adafd746273fcfaaafe0ffcc86b1558b
aybeedee <abdullahumer575@gmail.com>
2024-05-15 14:41:29 +0500

    feat: edit review functionality

--- backend/src/controllers/review.controller.ts
+++ backend/src/controllers/review.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { ReviewData } from "../utils/validations";
 import { validate } from "class-validator";
 import { Review } from "../models";
+import { Op } from "sequelize";
 
 /**
  * validate inputs, err if invalid |
@@ -41,6 +42,66 @@ export const addReview = async (req: Request, res: Response) => {
 					movieId: newReview.movieId,
 					comment: newReview.comment,
 					rating: newReview.rating,
+				},
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({
+			error: true,
+			message: "Internal server error",
+		});
+	}
+};
+
+/**
+ * validate inputs, err if invalid |
+ * get user id from req.body.userId |
+ * update review, return review |
+ */
+export const editReview = async (req: Request, res: Response) => {
+	try {
+		console.log(req.body);
+		const reviewData: ReviewData = new ReviewData();
+		reviewData.movieId = req.body.movieId;
+		reviewData.comment = req.body.comment;
+		reviewData.rating = req.body.rating;
+
+		const errors = await validate(reviewData);
+		if (errors.length > 0) {
+			return res.status(400).json({
+				error: true,
+				message: "Invalid input",
+				data: errors,
+			});
+		}
+
+		// update() does not return the record model, it returns affectedCount instead
+		// this is why returning updated result using data from the body below
+		await Review.update(
+			{
+				comment: reviewData.comment,
+				rating: reviewData.rating,
+			},
+			{
+				where: {
+					[Op.and]: {
+						userId: req.body.userId,
+						movieId: reviewData.movieId,
+					},
+				},
+			}
+		);
+
+		return res.status(200).json({
+			error: false,
+			message: "Review successfully updated",
+			data: {
+				review: {
+					userId: req.body.userId,
+					movieId: reviewData.movieId,
+					comment: reviewData.comment,
+					rating: reviewData.rating,
 				},
 			},
 		});
