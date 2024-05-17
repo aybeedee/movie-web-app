5b52a67bf9d1be6a9804455be8cf5106c401cc9c
aybeedee <abdullahumer575@gmail.com>
2024-05-17 09:54:40 +0500

    fix: center search bar when logged in

--- frontend/src/components/core/Navbar.tsx
+++ frontend/src/components/core/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Search } from "@/assets/icons";
+import { BackgroundIllustrationTop } from "@/assets/illustrations";
 import { useAuth } from "@/hooks";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -17,64 +18,71 @@ export default function Navbar() {
   }
 
   return (
-    <div className="flex flex-row justify-between items-center border border-green-500">
-      <h1 className="min-w-min">
-        {
-          isLoggedIn ?
-            `Welcome ${authInfo.user?.firstName}`
-            : "Welcome to CMDb"
-        }
-      </h1>
-      <div className="w-1/2 border border-red-500">
-        <form
-          className="relative w-full"
-          onSubmit={(event) => {
-            event.preventDefault();
-            // const newSearchParams = new URLSearchParams(searchParams);
-            // if (productSearchValue === "") {
-            //   newSearchParams.delete("search");
-            //   setSearchParams(newSearchParams);
-            // } else {
-            //   newSearchParams.set("search", productSearchValue);
-            //   setSearchParams(newSearchParams);
-            // }
-          }}
-        >
-          <div className="absolute left-2 top-[0.3rem] pr-2 border-r border-slate-200" >
-            <Search />
+    <div className="w-screen flex flex-row bg-[#18181a] border-b border-white/5">
+      <div className="w-full relative overflow-hidden">
+        <div className="absolute top-0 right-0 opacity-60 translate-x-1/2">
+          <BackgroundIllustrationTop />
+        </div>
+        <div className="flex flex-row justify-between items-center py-2 backdrop-blur-3xl">
+          <h1 className="min-w-min text-white">
+            {
+              isLoggedIn ?
+                `Welcome ${authInfo.user?.firstName}`
+                : "Welcome to CMDb"
+            }
+          </h1>
+          <div className="w-full fixed flex justify-center">
+            <form
+              className="relative w-1/3"
+              onSubmit={(event) => {
+                event.preventDefault();
+                // const newSearchParams = new URLSearchParams(searchParams);
+                // if (productSearchValue === "") {
+                //   newSearchParams.delete("search");
+                //   setSearchParams(newSearchParams);
+                // } else {
+                //   newSearchParams.set("search", productSearchValue);
+                //   setSearchParams(newSearchParams);
+                // }
+              }}
+            >
+              <div className="absolute left-2 top-[0.3rem] pr-2 border-r border-slate-200" >
+                <Search />
+              </div>
+              <input
+                className="pl-10 border border-slate-200 w-full"
+                type="text"
+              />
+            </form>
           </div>
-          <input
-            className="pl-10 border border-slate-200 w-full"
-            type="text"
-          />
-        </form>
-      </div>
-      <div className="flex flex-row">
-        {
-          isLoggedIn ?
-            <>
-              <p>
-                Logged in as {authInfo.user?.email}
-              </p>
-              <button
-                onClick={signout}
-                className="bg-black text-white w-fit px-4 py-1 rounded-sm">
-                Signout
-              </button>
-            </>
-            : <>
-              <Link
-                to="/login"
-                className="bg-black text-white w-fit px-4 py-1 rounded-sm">
-                Login
-              </Link>
-              <Link
-                to="/signup"
-                className="bg-black text-white w-fit px-4 py-1 rounded-sm">
-                Signup
-              </Link>
-            </>
-        }
+          <div className="flex flex-row z-10">
+            {
+              isLoggedIn ?
+                <>
+                  <p className="text-white">
+                    Logged in as {authInfo.user?.email}
+                  </p>
+                  <button
+                    onClick={signout}
+                    className="bg-black text-white w-fit px-4 py-1 rounded-sm">
+                    Signout
+                  </button>
+                </>
+                : <>
+                  <Link
+                    to="/login"
+                    className="bg-black text-white w-fit px-4 py-1 rounded-sm">
+                    Login
+                  </Link>
+                  <Link
+                    to="/signup"
+                    className="bg-black text-white w-fit px-4 py-1 rounded-sm">
+                    Signup
+                  </Link>
+                </>
+            }
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import { BackgroundIllustrationBottom, BackgroundIllustrationTop } from "@/assets/illustrations";
+
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="w-screen h-screen flex flex-row bg-[#18181a]">
+      <div className="w-full relative overflow-hidden">
+        <div className="absolute top-0 right-0 opacity-60 translate-x-1/2">
+          <BackgroundIllustrationTop />
+        </div>
+        <div className="absolute bottom-0 left-0 opacity-60 -translate-x-1/2">
+          <BackgroundIllustrationBottom />
+        </div>
+        <div className="flex flex-col gap-4 h-full justify-center items-center px-10">
+          <h1 className="text-white font-semibold text-4xl text-center">
+            Login to Your Account
+          </h1>
+          <p className="text-white text-lg text-center font-light">
+            Welcome Back! Login wtih your credentials to access all features of CMDb.
+          </p>
+          <div className="flex justify-center items-center gap-4">
+            <p className="text-white text-lg font-light">New Here?</p>
+
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
