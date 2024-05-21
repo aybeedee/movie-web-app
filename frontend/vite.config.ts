89e6470c2fe85e4036cbaabf4412019f5416bc60
aybeedee <abdullahumer575@gmail.com>
2024-05-21 15:41:10 +0500

    fix: vite build issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,13 +5,15 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-
+	server: {
+		host: true,
+		port: 8080,
+	},
 	build: {
 		sourcemap: true,
 	},
